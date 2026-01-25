ae856280891fcb5fe7847323e74a36977c4aba1b
Herdis Heida <herdisj24@ru.is>
2026-01-25 14:37:27 +0000

    little endian functionality

--- wav_file_generation.cpp
+++ wav_file_generation.cpp
@@ -24,20 +24,11 @@ void addSample(int wave, int sample) {
 }
 
 
-void writeInLittleEndian(unsigned char* out, int byteSize, const void* numberPtr) {
-    // TODO
-    (void) out;
-    (void) byteSize;
-    (void) numberPtr;
-
-    if (!out || !numberPtr || byteSize <= 0) return;
-
-    // if (byteSize == 1) out = &numberPtr;
-
-    // for (int i = 0; i < byteSize; i++) {
-    //     out[i] = &numberPtr[byteSize];
-    //     byteSize--;
-    // }
+void writeInLittleEndian(unsigned char* out, unsigned int value, int byteSize) {
+    for (int i = 0; i < byteSize; ++i) {
+        out[i] = (unsigned char)(value & 0xFF);
+        value >>= 8;
+    }
 }
 
 
