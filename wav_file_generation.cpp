d128e806b0dc37c5ace2020a1ee2793e8cec5cc7
Herdis Heida <herdisj24@ru.is>
2026-01-28 22:39:19 +0000

    remove comments

--- wav_file_generation.cpp
+++ wav_file_generation.cpp
@@ -25,56 +25,21 @@ static void makeWaveHeader( unsigned char header[44], unsigned int sampleRate, u
     
     unsigned int byteRate = sampleRate * numChannels * (bitsPerSample / 8);
     unsigned short blockAlign = (unsigned short) (numChannels * (bitsPerSample / 8));
-    
     unsigned int subchunk2Size = numSamples * numChannels * (bitsPerSample / 8);
     unsigned int chunkSize = 36 + subchunk2Size; // or 4 + (8 + SubChunk1Size) + (8 + SubChunk2Size)
 
-    
-    // RIFF HEADER
-
-    // 0–3 "RIFF"
     header[0] = 'R'; header[1] = 'I'; header[2] = 'F'; header[3] = 'F';
-
-    // 4–7 ChunkSize
     writeUIntLE(header + 4, chunkSize, 4);
-
-    // 8–11 "WAVE"
     header[8]  = 'W'; header[9]  = 'A'; header[10] = 'V'; header[11] = 'E';
-
-
-    // SUBCHUNKS - Subchunk 1
-
-    // 12–15 "fmt "
     header[12] = 'f'; header[13] = 'm'; header[14] = 't'; header[15] = ' ';
-
-    // 16–19 Subchunk1Size (16 for PCM)
     writeUIntLE(header + 16, 16, 4);
-
-    // 20–21 AudioFormat (1 = PCM)
     writeUIntLE(header + 20, 1, 2);
-
-    // 22–23 NumChannels (Mono = 1, Stereo = 2, etc)
     writeUIntLE(header + 22, numChannels, 2);
-
-    // 24–27 SampleRate (8000, 44100, etc)
     writeUIntLE(header + 24, sampleRate, 4);
-
-    // 28–31 ByteRate
     writeUIntLE(header + 28, byteRate, 4);
-
-    // 32–33 BlockAlign
     writeUIntLE(header + 32, blockAlign, 2);
-
-    // 34–35 BitsPerSample
     writeUIntLE(header + 34, bitsPerSample, 2);
-
-
-    // SUBCHUNKS - Subchunk 1
-
-    // 36–39 "data"
     header[36] = 'd'; header[37] = 'a'; header[38] = 't'; header[39] = 'a';
-
-    // 40–43 Subchunk2Size
     writeUIntLE(header + 40, subchunk2Size, 4);
 }
 
